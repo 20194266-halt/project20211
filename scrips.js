6948c1c2ae2af69226ecd04095aecfff5b3cca9b
hala194266 <91833527+hala194266@users.noreply.github.com>
2021-10-29 22:05:08 +0700

    Update scrips.js

--- scrips.js
+++ scrips.js
@@ -2,16 +2,7 @@
 function myFunction() {
     document.getElementById("home").innerHTML = "Welcome to JavaScript!";
   }
-  function myFunction() {
-    document.getElementById("home").innerHTML = "I love learning JavaScript!";
-  }
-function myFunction() {
-    document.getElementById("home").innerHTML = "I love learning JavaScript!";
-  }
   
-function myFunction() {
-  document.getElementById("home").innerHTML = "Welcome to JavaScript!";
-}
   
   
   
