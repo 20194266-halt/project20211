c023941e1659fff271cdb8020f090710184c2570
hala194266 <ha.lt194266@sis.hust.edu.vn>
2021-10-29 22:10:12 +0700

    sua de tao conflict

--- scrips.js
+++ scrips.js
@@ -9,9 +9,7 @@ function myFunction() {
     document.getElementById("home").innerHTML = "I love learning JavaScript!";
   }
   
-function myFunction() {
-  document.getElementById("home").innerHTML = "Welcome to JavaScript!";
-}
+
   
   
   
