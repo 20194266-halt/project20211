a2282ec3095579047d849eae8e00fa0d27c648cd
20194266-halt <91833527+20194266-halt@users.noreply.github.com>
2021-11-12 13:09:09 +0700

    Create hala.js

---
+++ 2019666/hala.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const session = require('express-session');
+const flash = require('express-flash');
+const passport = require('passport');
+const initializePassport = require("./passportConfig");
+
+initializePassport(passport);
+
+const app = express();
+
+app.set("view engine", 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({extended: false}));
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+const user = require('./routes/user');
+const map = require('./routes/map');
+const infor = require('./routes/infor');
+
+//user
+app.use('/', user);
+app.use('/map', map);
+app.use('/infor', infor); //Thông tin tài khoản
+// app.use('/history', history);
+
+//admin
+
+app.listen((process.env.PORT || 3000), (req, res) => {
+    console.log("Run in port 3000");
+});
